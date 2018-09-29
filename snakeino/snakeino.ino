145dd134ca2a8b9ca225d464256beda7b41b1a57
antoniobutkovic <antonio.butkoviic@gmail.com>
2018-09-29 11:56:36 +0200

    Added function for joystick directions reading

--- snakeino/snakeino.ino
+++ snakeino/snakeino.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+const int SW_pin = 2;
+const int X_pin = 0;
+const int Y_pin = 1;
 
+void setup() {
+  pinMode(SW_pin, INPUT);
+  digitalWrite(SW_pin, HIGH);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  char getDirection();
+
+  Serial.print(getDirection());
+  delay(500);
+}
+
+char getDirection() {
+  if (analogRead(X_pin) == 1023) {
+    return 'u';
+  }else if (analogRead(X_pin) == 0) {
+    return 'd';
+  } else if (analogRead(Y_pin) == 0) {
+    return 'l';
+  } else if (analogRead(Y_pin) == 1023) {
+    return 'r';
+  }
+ 
 }
