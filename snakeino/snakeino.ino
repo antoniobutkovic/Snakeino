c9467fd50633b13f48707e5915d88402d7753e7e
antoniobutkovic <antonio.butkoviic@gmail.com>
2018-09-29 14:31:19 +0200

    Connected 8x8 board, included LedControl library, displayed test dot on board

--- snakeino/snakeino.ino
+++ snakeino/snakeino.ino
@@ -1,30 +1,53 @@
+#include "LedControl.h"
+
+// pins
 const int SW_pin = 2;
 const int X_pin = 0;
 const int Y_pin = 1;
+const int DIN = 12;
+const int CS = 11;
+const int CLK = 10;
+
+//variables
+const int screenWidth = 8;
+const int screenHeight = 8;
+
+LedControl lc = LedControl(DIN, CS, CLK, 1);
 
 void setup() {
+  setupPins();
+  setupLedBoard();
+  drawSnake();
+}
+
+void setupLedBoard() {
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 8);
+  lc.clearDisplay(0);
+}
+
+void setupPins() {
   pinMode(SW_pin, INPUT);
   digitalWrite(SW_pin, HIGH);
-  Serial.begin(9600);
 }
 
 void loop() {
-
   char getDirection();
-
-  Serial.print(getDirection());
-  delay(500);
 }
 
 char getDirection() {
   if (analogRead(X_pin) == 1023) {
     return 'u';
-  }else if (analogRead(X_pin) == 0) {
+  } else if (analogRead(X_pin) == 0) {
     return 'd';
   } else if (analogRead(Y_pin) == 0) {
     return 'l';
   } else if (analogRead(Y_pin) == 1023) {
     return 'r';
   }
- 
+  return 'n';
+}
+
+void drawSnake() {
+  lc.setLed(0, 2, 2, true);
 }
