c4c2cecb66942d4db637acc971ae9fd32179b0da
antoniobutkovic <antonio.butkoviic@gmail.com>
2018-09-29 16:27:58 +0200

    Added snake food, managed to control snake with joystick

--- snakeino/snakeino.ino
+++ snakeino/snakeino.ino
@@ -11,13 +11,28 @@ const int CLK = 10;
 //variables
 const int screenWidth = 8;
 const int screenHeight = 8;
+int snakeX, snakeY, foodX, foodY, score;
+char direction = 'u';
 
 LedControl lc = LedControl(DIN, CS, CLK, 1);
 
 void setup() {
+  Serial.begin(9600);
   setupPins();
   setupLedBoard();
+  setupSnakePosition();
+  setupFoodPosition();
   drawSnake();
+}
+
+void setupSnakePosition() {
+  snakeX = 3;
+  snakeY = 5;
+}
+
+void setupFoodPosition() {
+  foodX = 5;
+  foodY = 3;
 }
 
 void setupLedBoard() {
@@ -32,22 +47,59 @@ void setupPins() {
 }
 
 void loop() {
-  char getDirection();
+  changeSnakeDirection(getJoystickDirection());
+  drawSnake();
+  delay(1000);
 }
 
-char getDirection() {
+char getJoystickDirection() {
   if (analogRead(X_pin) == 1023) {
-    return 'u';
+    direction = 'u';
   } else if (analogRead(X_pin) == 0) {
-    return 'd';
-  } else if (analogRead(Y_pin) == 0) {
-    return 'l';
+    direction = 'd';
   } else if (analogRead(Y_pin) == 1023) {
-    return 'r';
+    direction = 'l';
+  } else if (analogRead(Y_pin) == 0) {
+    direction = 'r';
   }
-  return 'n';
+  return direction;
+}
+
+void changeSnakeDirection(char direction) {
+  switch (direction) {
+    case 'l':
+      snakeX--;
+      break;
+    case 'r':
+      snakeX++;
+      break;
+    case 'u':
+      snakeY--;
+      break;
+    case 'd':
+      snakeY++;
+      break;
+  }
+}
+
+void showLed(int row, int column) {
+  lc.setLed(0, row, column, true);
+}
+
+void hideLed(int row, int column) {
+  lc.setLed(0, row, column, false);
 }
 
 void drawSnake() {
-  lc.setLed(0, 2, 2, true);
+  for (int i = 0; i < screenHeight; i++) {
+    for (int j = 0; j < screenWidth; j++) {
+      if (i == snakeY && j == snakeX) {
+        showLed(snakeX, snakeY);
+      } else if (i == foodY && j == foodX) {
+        showLed(foodX, foodY);
+      } else {
+        hideLed(j, i);
+      }
+    }
+  }
 }
